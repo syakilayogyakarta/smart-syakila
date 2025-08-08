5f997b0627b9cfb6c49f1bbb0cd6950e055b0c1a
Syakila Yogyakarta <syakilayogyakarta@gmail.com>
2025-08-08 12:28:32 +0000

    Baik saya akan berikan database siswa
    berikut ini nama, nama panggilan,

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,21 +1,70 @@
+
 export const facilitator = {
     name: "Ahmad Dahlan",
-    class: "Kelas C",
+    class: "Kelas Ta'lim 1",
   };
   
-  export const classes = ["Kelas A", "Kelas B", "Kelas C", "Kelas D"];
+  export const classes = ["Kelas Tarbiyah", "Kelas Ta'dib", "Kelas Ta'lim 1", "Kelas Ta'lim 2"];
   
   export const studentsByClass: { [key: string]: string[] } = {
-    "Kelas A": ["Adi Saputra", "Budi Santoso", "Citra Lestari", "Dewi Anggraini", "Eka Wijaya"],
-    "Kelas B": ["Fajar Nugroho", "Gita Puspita", "Hendra Gunawan", "Indah Permata", "Joko Susilo"],
-    "Kelas C": ["Kartika Sari", "Lia Amelia", "Muhammad Rizky", "Nadia Fitriani", "Putri Ayu"],
-    "Kelas D": ["Rian Hidayat", "Siti Nurhaliza", "Teguh Prakoso", "Utami Dewi", "Yoga Pratama"],
+    "Kelas Tarbiyah": [
+        "Muhammad Abdan Khioiri Tsabit",
+        "Muhammad Aqifan al-Fatih",
+        "Muhammad Irhab Mirza",
+        "Yahya Ayyasyh Satriawan Hidayat",
+    ],
+    "Kelas Ta'dib": [
+        "Athaniya Ghina Rafifa",
+        "Avicenna Akthar Dhiyaulhaq",
+        "Azzam Muhammad Matitaputi",
+        "Nailah Amirah Khoirunnisa'",
+        "Ulin Najwa Nafi'a Ashari",
+    ],
+    "Kelas Ta'lim 1": [
+        "Bima Andi Satria",
+        "Ibrahim Viday Hafuza",
+        "Muhammad Ismail Al-Fatih",
+        "Muhammad Ziyad Dhiyaurrahman",
+        "Rifa'a Windy Nur Pujiutami",
+        "Rofi'i Sindy Nur Pujiutami",
+        "Assafa Octaviola Putri Ismawan",
+    ],
+    "Kelas Ta'lim 2": [
+        "Mayyada Zuhro",
+        "Muhammad Hajid Al-Miqdad",
+        "Muhammad Nabil",
+        "Muhammad Nuhaa Naufali Ar-Rasis",
+    ],
   };
+
+  export const studentDetails: { [fullName: string]: any } = {
+    "Muhammad Abdan Khioiri Tsabit": { nickname: "Abdan", dob: "14 November 2012" },
+    "Muhammad Aqifan al-Fatih": { nickname: "Aqif", dob: "12 April 2012" },
+    "Muhammad Irhab Mirza": { nickname: "Irhab", dob: "25 Mei 2013" },
+    "Yahya Ayyasyh Satriawan Hidayat": { nickname: "Ayyash", dob: "31 Maret 2011" },
+    "Athaniya Ghina Rafifa": { nickname: "Ghina", dob: "27 Februari 2012" },
+    "Avicenna Akthar Dhiyaulhaq": { nickname: "Akhtar", dob: "10 Agustus 2011" },
+    "Azzam Muhammad Matitaputi": { nickname: "Azzam", dob: "1 Maret 2011" },
+    "Nailah Amirah Khoirunnisa'": { nickname: "Amirah", dob: "11 Desember 2010" },
+    "Ulin Najwa Nafi'a Ashari": { nickname: "Najwa", dob: "4 Januari 2011" },
+    "Bima Andi Satria": { nickname: "Andi", dob: "18 Juni 2009" },
+    "Ibrahim Viday Hafuza": { nickname: "Fuza", dob: "6 Desember 2008" },
+    "Muhammad Ismail Al-Fatih": { nickname: "Alfath", dob: "20 Agustus 2010" },
+    "Muhammad Ziyad Dhiyaurrahman": { nickname: "Ziyad", dob: "26 April 2009" },
+    "Rifa'a Windy Nur Pujiutami": { nickname: "Indy", dob: "31 Agustus 2009" },
+    "Rofi'i Sindy Nur Pujiutami": { nickname: "Fii", dob: "31 Agustus 2009" },
+    "Assafa Octaviola Putri Ismawan": { nickname: "Assa", dob: "11 Oktober 2010" },
+    "Mayyada Zuhro": { nickname: "May", dob: "1 Desember 2007" },
+    "Muhammad Hajid Al-Miqdad": { nickname: "Hajid", dob: "11 Juni 2008" },
+    "Muhammad Nabil": { nickname: "Nabil", dob: "4 Maret 2008" },
+    "Muhammad Nuhaa Naufali Ar-Rasis": { nickname: "Nuha", dob: "16 Juni 2006" },
+  };
+
   
   export const studentProfile = {
-    fullName: "Syakila Putri",
-    nickname: "Syakila",
-    nisn: "1234567890",
+    fullName: "Muhammad Abdan Khioiri Tsabit",
+    nickname: "Abdan",
+    nisn: "0012111401",
     photoUrl: "https://placehold.co/100x100.png",
     photoHint: "student portrait",
     attendance: {
